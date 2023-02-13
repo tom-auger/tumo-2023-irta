c7e50ad4309f235c3d0619e7a8a2fb38d778219b
Tom Auger <tomauger@live.com>
2023-02-13 17:32:06 +0400

    Add to setup

--- setup.sh
+++ setup.sh
@@ -2,6 +2,6 @@
 
 conda create --name tumo-irta python=3.10 notebook numpy matplotlib scikit-learn
 conda activate tumo-irta
-conda install -c conda-forge gensim
+conda install -c conda-forge gensim ipython_unittest
 pip install pystemmer
 
